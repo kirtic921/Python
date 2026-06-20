a191c1098594c201eaf344a9e3a1f054e01e370b
kirtic921 <kirtic921@gmail.com>
2026-06-20 14:51:12 +0530

    Modified ans03.py

---
+++ 02_conditionals/03-ans.py
@@ -0,0 +1,20 @@
+# Grade Calculator
+# Assign grade according to score: A(90-100), B(80-89), C(70-79), D(60-69), E(50-59), F(<50)
+
+score = input("Enter your marks:")
+iscore= int(score)
+
+if(iscore>=90):
+    grade='A'
+elif(iscore>=80 and iscore<=89):
+    grade='B' 
+elif(iscore>=70 and iscore<=79):
+    grade='C' 
+elif(iscore>=60 and iscore<=69):
+    grade='D'
+elif(iscore>=50 and iscore<=59):
+    grade='E'
+else:
+    grade='Fail'
+
+print("Your Grade: ", grade)  
