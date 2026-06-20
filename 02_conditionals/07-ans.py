f34b0bde986f3f006d4dca2eb4af7b910ee022f3
kirtic921 <kirtic921@gmail.com>
2026-06-20 17:56:03 +0530

    added ans07.py

---
+++ 02_conditionals/07-ans.py
@@ -0,0 +1,14 @@
+# Coffee Customization
+# Problem: Customize a coffee order: "Small", "Medium" or "Large" with an option of "Extra shot" of espresso.
+
+coffee_size='Large'
+extra_shot=False
+
+if(extra_shot):
+    output = coffee_size + ' Coffee with an extra shot'
+else:
+    output = coffee_size + ' Coffee with no extra shot'
+
+print(output)    
+
+    
