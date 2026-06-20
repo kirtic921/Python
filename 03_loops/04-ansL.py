352fe6b411badb5b3f94f34eebe46108ec042fa2
kirtic921 <kirtic921@gmail.com>
2026-06-20 21:50:10 +0530

    modified ans04l.py

--- 03_loops/04-ansL.py
+++ 03_loops/04-ansL.py
@@ -2,14 +2,19 @@
 # Problem: Reverse a string using loop.
 
 str=input("Enter a string: ")
-print(str)
-n=len(str)
-series= range(0, n)
+# Method-1
+# print(str)
+# n=len(str)
+# series= range(0, n)
+# output=''
+
+# for i in series:
+#     output+=str[n-i-1]
+
+# METHOD-2
 output=''
-
-for i in series:
-    output+=str[n-i-1]
-
+for char in str:
+    output=char+output
 
 print("The reversed string :", output)
 
