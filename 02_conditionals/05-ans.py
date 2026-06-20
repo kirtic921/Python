428a0c9eaa9c5b61be935d563fd5de2e86c334ed
kirtic921 <kirtic921@gmail.com>
2026-06-20 16:44:58 +0530

    Added ans05.py

---
+++ 02_conditionals/05-ans.py
@@ -0,0 +1,13 @@
+# Weather Activity Suggestion
+# Problem: Suggest an activity based on weather 
+
+weather=input("Enter the current weather : ")
+
+if(weather=='sunny'):
+    print("Go for a walk.")
+elif(weather=='rainy'):
+    print("Read a book.")
+elif(weather=='snowy'):
+    print("Build a snowman")
+else:
+    print("Plwase enter a valid weather")
