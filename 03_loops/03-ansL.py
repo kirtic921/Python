7db4a2339b607c47474e86190eb57eebba41aef9
kirtic921 <kirtic921@gmail.com>
2026-06-20 21:07:50 +0530

    minor changes done in ans03l.py

--- 03_loops/03-ansL.py
+++ 03_loops/03-ansL.py
@@ -8,9 +8,16 @@ series = range(0, 11)
 
 print("The Multiplication table for the entered value (skipping the fifth iteration) is as follows: \n")
 
+# Method-1
+# for i in series:
+#     if(i!=5):
+#         print(i_n*i)
+
+# Method 2
 for i in series:
-    if(i!=5):
-        print(i_n*i)
-    
+    if(i==5):
+        continue
+        
+    print(i_n,'X', i, '=', i_n*i)    
 
     
