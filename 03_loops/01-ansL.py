3d075a2b00706962fd13495da1502783bc224a40
kirtic921 <kirtic921@gmail.com>
2026-06-20 19:38:54 +0530

    added ansl-01.py

---
+++ 03_loops/01-ansL.py
@@ -0,0 +1,11 @@
+# Counting positive numbers
+# Problem: Given a list of numbers, count how many are positive.
+
+numbers = [1, -2, 3, -4, 5, 6, -7, -8, 9, 10]
+count=0
+
+for i in numbers:
+    if(i>=0):
+        count=count+1
+
+print("Total positive numbers in the provided list are:", count)
