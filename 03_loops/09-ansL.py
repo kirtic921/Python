e61eeb1669a386898680de0919b0307b1b47c3b9
kirtic921 <kirtic921@gmail.com>
2026-06-21 16:41:17 +0530

    modified ans09l.py

--- 03_loops/09-ansL.py
+++ 03_loops/09-ansL.py
@@ -2,12 +2,24 @@
 # Problem: Check if all elements in a list are unique. If duplicate is found, exit the loop and print the duplicate.
 
 items =["apple", "banana", "orange", "orange", "mango"]
-n=len(items)
 
-for i in range(0,n):
-    curr=items[i]
-    for k in range(0,n):
-        if(i!=k):
-            if(curr==items[k]):
-                print(curr)
-                break
+# MY METHOD
+# n=len(items)
+
+# for i in range(0,n):
+#     curr=items[i]
+#     for k in range(0,n):
+#         if(i!=k):
+#             if(curr==items[k]):
+#                 print(curr)
+#                 break
+
+# Method-2
+unique_item=set()
+
+for item in items:
+    if (item in unique_item):
+        print("Duplicate: ", item)
+        break
+    else:
+        unique_item.add(item)    
