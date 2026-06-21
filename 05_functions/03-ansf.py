4321fbc906f8473e79ab12b886be078101f22c99
kirtic921 <kirtic921@gmail.com>
2026-06-22 01:36:38 +0530

    3functions

---
+++ 05_functions/03-ansf.py
@@ -0,0 +1,7 @@
+# Polymorphism in Functions
+# Problem: Write a function multiply that multiplies two numbers (5*5), but can also accept and multipy strings with numbers("K"*5).
+
+def multiply(a,b):
+    return a*b
+
+print(multiply("KirtiChoudhary",2))
