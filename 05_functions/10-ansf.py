eb831e413e5589de1f99c2c5adaac7d9a930c3ab
kirtic921 <kirtic921@gmail.com>
2026-06-22 14:11:56 +0530

    10functions

--- 05_functions/10-ansf.py
+++ 05_functions/10-ansf.py
@@ -1,2 +1,11 @@
 # Recursive function
 # Problem: Create a recursive function that will calculate the factorial of a number.
+
+def fact(n):
+    if(n!=1):
+        return n*fact(n-1)
+    elif(n==1):
+        return 1
+
+print(fact(5))
+            
