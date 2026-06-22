7d1a5c993201e00e93b6b8d61ceb7ce634c1dde3
kirtic921 <kirtic921@gmail.com>
2026-06-23 01:40:34 +0530

    instances of classess

--- 07_oop/ans-01o.py
+++ 07_oop/ans-01o.py
@@ -2,8 +2,17 @@
 # Problem: Create a Car class with attributes like brand and model. Then create an instance of this class.
 
 class Car:
-    brand=None
-    model=None
+    def __init__(self, brand, model):
+        self.brand=brand
+        self.model=model
 
-my_car = Car()
-print(my_car)
+my_car=Car("Toyota", "Corolla")
+print(my_car.brand)
+print(my_car.model)
+
+# NOT A GOOD WAY -cant pass parameters while forming instances
+# class Car:
+#     brand=None
+#     model=None
+# my_car = Car()
+# print(my_car)
