df7f65cbe7ee1dc946e7e77252d7d9a4c3ee3774
kirtic921 <kirtic921@gmail.com>
2026-06-22 13:13:41 +0530

    8functions

--- 05_functions/08-ansf.py
+++ 05_functions/08-ansf.py
@@ -1,2 +1,8 @@
 # Function with **kwargs
 # Problem: Create a function that accepts any number of keywords and prints them in format key:value
+
+def print_kwargs(**kwargs):
+    for key, value in kwargs.items():
+        print(f"{key}:{value}")
+
+print_kwargs(name="Shaktiman", power="laser", enemy="Dr. Jackaal")
