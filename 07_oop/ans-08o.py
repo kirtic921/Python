e9de582b263686ff35759f29add928fc2d014f37
kirtic921 <kirtic921@gmail.com>
2026-06-23 21:44:42 +0530

    property decorators

--- 07_oop/ans-08o.py
+++ 07_oop/ans-08o.py
@@ -1,3 +1,15 @@
 # Property decorators
 # Problem: Use a property decorator in the class Car to make the model attribute read only.
 
+class Car:
+    def __init__(self, brand, model):
+        self.brand=brand
+        self.__model=model
+    @property
+    def read_model(self):
+        return self.__model
+
+
+my_car=Car("Toyota", "Corolla")
+my_car.Car="Model-s"
+print(my_car.read_model)
