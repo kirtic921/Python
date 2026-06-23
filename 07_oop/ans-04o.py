5fc96f92c105ee75865b9a3d8072f341a643b533
kirtic921 <kirtic921@gmail.com>
2026-06-23 11:18:41 +0530

    encapsulation added

--- 07_oop/ans-04o.py
+++ 07_oop/ans-04o.py
@@ -1,2 +1,20 @@
 # Encapsulation
-# Problem: Modify the Car class to encapsulate the brand attribute, making it private, and provide a better method for it.
+# Problem: Modify the Car class to encapsulate the brand attribute, making it private, and provide a getter method for it.
+
+class Car:
+    def __init__(self, brand, model):
+        self.__brand=brand
+        self.model=model
+    def get_brand(self):
+        return self.__brand
+
+class ElectricCar(Car):
+    def __init__(self, brand, model, battery_size):
+        super().__init__(brand, model)
+        # instead of again writing self.brand and  self.model n all, write the above command
+        self.battery_size=battery_size
+
+
+my_tesla=ElectricCar("Tesla", "Model-s", "346kwwh")
+# print(my_tesla.__brand)
+print(my_tesla.get_brand())
