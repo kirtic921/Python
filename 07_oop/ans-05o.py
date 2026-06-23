87b4358f6030f987630a5c5560fd2ffb86550593
kirtic921 <kirtic921@gmail.com>
2026-06-23 12:30:59 +0530

    polymorphism

--- 07_oop/ans-05o.py
+++ 07_oop/ans-05o.py
@@ -1,2 +1,25 @@
 # Polymorphism
 # Problem: Demonstrate polymorphism by defining a method fuel_type in both Car and ElectricCar classes, but with different behaviors.
+
+class Car:
+    def __init__(self, brand, model):
+        self.brand=brand
+        self.model=model
+    def fuel_type(self):
+        return "petrol and diesel"
+    
+
+class ElectricCar(Car):
+    def __init__(self, brand, model, battery_size):
+        super().__init__(brand, model)
+        # instead of again writing self.brand and  self.model n all, write the above command
+        self.battery_size=battery_size
+    def fuel_type(self):
+        return "electric charge"
+
+
+my_car=Car("Tata", "Safari")
+print(my_car.fuel_type())
+
+my_tesla=ElectricCar("Tesla", "Model-s", "346kwwh")
+print(my_tesla.fuel_type())
