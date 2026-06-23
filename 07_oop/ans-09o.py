dbc3f8fcf89cbeb45c327306fe19114f8a78d2cd
kirtic921 <kirtic921@gmail.com>
2026-06-23 22:25:59 +0530

    isinstance() tested successfully

--- 07_oop/ans-09o.py
+++ 07_oop/ans-09o.py
@@ -1,2 +1,19 @@
 # Class Inheritance and isinstance() Function 
 # Problem: Demonstarte the use of isinstance() to check my_tesla is an instance of Car and ElectricCar.
+
+class Car:
+    def __init__(self, brand, model):
+        self.brand=brand
+        self.model=model
+
+class ElectricCar(Car):
+    def __init__(self, brand, model, battery_size):
+        super().__init__(brand, model)
+        # instead of again writing self.brand and  self.model n all, write the above command
+        self.battery_size=battery_size
+
+
+my_tesla=ElectricCar("Tesla", "Model-s", "346kwwh")
+
+print(isinstance(my_tesla, Car))
+print(isinstance(my_tesla, ElectricCar))
