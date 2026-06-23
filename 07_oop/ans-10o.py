e79139bae015473ca436252558c074628b701b10
kirtic921 <kirtic921@gmail.com>
2026-06-24 00:48:54 +0530

    multiple inheritance handelled successfully

--- 07_oop/ans-10o.py
+++ 07_oop/ans-10o.py
@@ -1,2 +1,19 @@
 # Multiple Inheritance
 # Problem: Create two classes Battery and Engine, and let the ElectricCar class inherit from both, demostrating  multiple inheritance.
+
+class Battery:
+    def __init__(self, battery):
+        self.battery=battery
+
+class Engine:
+    def __init__(self, engine):
+        self.engine=engine
+
+class ElectricCar(Battery, Engine):
+    def __init__(self, battery, engine, battery_size):
+        Battery.__init__(self,battery)
+        Engine.__init__(self, engine)
+        self.battery_size=battery_size
+
+my_car=ElectricCar("BATTERY", "ENGINE", "SIZE")
+print(my_car.battery)
