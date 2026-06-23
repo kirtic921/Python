c351df66a0da564c8abc8b32a209f0e8e781d946
kirtic921 <kirtic921@gmail.com>
2026-06-23 20:03:14 +0530

    class variables

--- 07_oop/ans-06o.py
+++ 07_oop/ans-06o.py
@@ -1,3 +1,14 @@
 # Class Variables
 # Problem: Add a class variable to Car that keeps track of the number of cars created.
 
+class Car:
+    total_cars=0
+    def __init__ (self, brand, model):
+        self.brand=brand
+        self.model=model
+        Car.total_cars+=1
+
+Car("Tata", "Safari")
+Car("Tesla", "Model-s")
+
+print(Car.total_cars)
