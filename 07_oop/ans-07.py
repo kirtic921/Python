6ffe2237ce53ab4dbf917ee27a750e729bcf47b6
kirtic921 <kirtic921@gmail.com>
2026-06-23 20:39:50 +0530

    static method

--- 07_oop/ans-07.py
+++ 07_oop/ans-07.py
@@ -1,3 +1,18 @@
 # Static method
 # Problem: Add a static method to the Car class that returns a general description of a car.
 
+class Car:
+    def __init__(self, brand, model):
+        self.brand=brand
+        self.model=model
+    @staticmethod
+    def description():                       
+        return "Cars are machines"
+
+
+# here, didnt put (self) only (), as staticmethod already can be accessed via classes only and not by objects no need to link it using self
+
+
+my_car=("Tesla", "Model-s")
+# print(my_car.description())
+print(Car.description())
