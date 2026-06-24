c0a87479970c073e6eb9097ee6f25afed499d868
kirtic921 <kirtic921@gmail.com>
2026-06-24 11:39:53 +0530

    decorator-one

--- 08_decorators/ans-01d.py
+++ 08_decorators/ans-01d.py
@@ -1,2 +1,19 @@
 # Timing Function Execution
 # Problem: Write a decorator that measures the time a function takes to execute.
+
+import time
+
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start=time.time()
+        result = func(*args, **kwargs)
+        end=time.time()
+        print(f"{func.__name__} ran in {end-start} time.")
+        return result
+    return wrapper
+
+@timer
+def eg_func(n):
+    time.sleep(n)
+
+eg_func(4)
