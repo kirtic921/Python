13c69f80c5c17d2b4c7ea2e31c630be4b6893169
kirtic921 <kirtic921@gmail.com>
2026-06-24 16:13:18 +0530

    3d added

---
+++ 08_decorators/ans-03d.py
@@ -0,0 +1,2 @@
+# Cache Return values
+# Problem: Implement a decorator that caches the return values of a function, so that when it's called with the same arguments, the cached value is returned instead of re-executing the function.
