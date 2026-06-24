10607566d65574e46d1eeb19cc223d5ec5708c7c
kirtic921 <kirtic921@gmail.com>
2026-06-24 13:07:01 +0530

    mine solution to 2decorator

--- 08_decorators/ans-02d.py
+++ 08_decorators/ans-02d.py
@@ -1,3 +1,17 @@
 # Debugging Function Calls 
 # Create a decorator to print the function name and the values of its arguments every time the function is called.
 
+def toll_function(func):
+    def wrapper(*args, **kwargs):
+        result=func(*args, **kwargs)
+        print(f"Function name is: {func.__name__} & arguments passed are: {args}")
+        return result
+    return wrapper
+
+@toll_function
+def main_function(a,b):
+    print("Sum of parameters entered is: ", a+b)
+
+main_function(5,3)
+
+# MINE SOUTION
